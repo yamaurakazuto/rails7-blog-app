a616fe6bae5e9d3fd3458d9459dbdc884bd25af6
kazuto <xyst510@icloud.com>
2024-06-02 19:54:46 +0900

    cssの仕上げ

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+    def new
+        super
+      end
+    
+      def create
+        super
+      end
+    
+      def update
+        super
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  
   root to: 'home#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
